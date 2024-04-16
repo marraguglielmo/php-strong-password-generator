bb6803802e778936870d035deec71920c4b4e374
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-16 15:59:55 +0200

    [fix] function

--- data/function.php
+++ data/function.php
@@ -8,6 +8,11 @@ function getRandomPass($password_length, $symbols, $lettersMin, $lettersMai, $nu
     if (isset($password_length) && $password_length > 5 && $password_length <= 25) {
         $password = true;
         $allCharacters = array_merge($symbols, $lettersMin, $lettersMai, $numbers);
+        $randPassword = [];
+        for ($i = 0; $i <= $password_length; $i++) {
+            $randPassword = array_rand($allCharacters, 1);
+        }
+        var_dump($allCharacters[$randPassword]);
     }
 
     return $password;
